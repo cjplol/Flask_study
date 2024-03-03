81d4281a9ea566ae2eba951985bda0c93020b664
cjplol <1430417057@qq.com>
2024-03-03 11:11:42 +0800

    .

--- button.py
+++ button.py
@@ -6,5 +6,9 @@ app=Flask(__name__)
 def show_button_index():
     return render_template("button.html")
 
+@app.route('/index.html')
+def show_index():
+    return render_template('button_index.html')
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=2145)
--- static/js/app.js
+++ static/js/app.js
@@ -2,4 +2,16 @@ function validateForm(){
     var username=document.getElementById('username').value;
     var password=document.getElementById('password').value;
     console.log("账号为："+username,"密码为："+password)
+    if (username.trim() === '' || password.trim() === '') {
+                alert('账号和密码不能为空！');
+                return false;
+            }
+    else {
+        localStorage.setItem('username',username)
+        alert('登录成功！');
+        // 这里可能还需要其它操作，比如提交表单
+        window.location.href='index.html';
+    }
+
+    return true; // 表示表单验证通过
 }
